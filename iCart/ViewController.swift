7ebbb0bca85232ca6b0eee89032fb5afdd763b6c
Shreyash <shreyashp47@gmail.com>
2024-01-12 07:44:09 +0530

    Initial Commit

---
+++ iCart/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iCart
+//
+//  Created by Shreyash Pattewar on 12/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
